3d73896cc9e9a9f2741417632d8dcbc6097d59ba
thelicato <thelicato@users.noreply.github.com>
2023-11-09 00:22:46 +0100

    feat: add auth to flag manager

--- Dummy/service/src/server.js
+++ Dummy/service/src/server.js
@@ -1,65 +1,85 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const flagService = require('./flag')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const flagService = require("./flag");
+const app = express();
 
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 PORT = 5001;
 
 const log = (msg) => {
-	const currentDate = new Date().toUTCString();
-	console.log(`[${currentDate}] ${msg}`)
-}
+  const currentDate = new Date().toUTCString();
+  console.log(`[${currentDate}] ${msg}`);
+};
 
-app.get('/', (_req, res) => {
-	log("GET on /")
-	res.send('I am just a dummy Vulnbox')
-})
+app.get("/", (_req, res) => {
+  log("GET on /");
+  res.send("I am just a dummy Vulnbox");
+});
 
-app.get('/ping', (_req, res) => {
-	log("GET on /ping")
-	res.send('I am just a dummy Vulnbox')
-})
+app.get("/ping", (_req, res) => {
+  log("GET on /ping");
+  res.send("I am just a dummy Vulnbox");
+});
 
-app.get('/get', (req, res) => {
-	log("GET on /get")
+app.get("/get", (req, res) => {
+  log("GET on /get");
 
-	if (req.query && req.query.id && req.query.vuln) {
-		log(`Received id: ${req.query.id} - vuln: ${req.query.vuln}`)
-		const id = req.query.id
-		const vuln = req.query.vuln
-		res.json({flag:flagService.getFlag(id, vuln)})
-	}
-	else {
-		res.send("ERROR: flag not set")
-	}
-})
+  const expectedAuth = `Bearer ${process.env.FLAG_MANAGER_SECRET}`;
+  if (
+    !req.headers ||
+    !req.headers.authorization ||
+    req.headers.authorization !== expectedAuth
+  ) {
+    res.send("Unauthorized");
+  }
 
-app.post('/put', (req, res) => {
-	log("POST on /put")
+  if (req.query && req.query.id && req.query.vuln) {
+    log(`Received id: ${req.query.id} - vuln: ${req.query.vuln}`);
+    const id = req.query.id;
+    const vuln = req.query.vuln;
+    res.json({ flag: flagService.getFlag(id, vuln) });
+  } else {
+    res.send("ERROR: unable to get flag");
+  }
+});
 
-	if (req.body && req.body.id && req.body.vuln && req.body.flag) {
-		log(`Received body with id: ${req.body.id} - vuln: ${req.body.vuln} - flag: ${req.body.flag}`)
-		const id = req.body.id
-		const vuln = req.body.vuln
-		const flag = req.body.flag
+app.post("/put", (req, res) => {
+  log("POST on /put");
 
-		flagService.setFlag(id, vuln, flag)
-		res.send("Flag correctly set")
-	}
-	else {
-		res.send("ERROR: flag not set")
-	}
-})
+  const expectedAuth = `Bearer ${process.env.FLAG_MANAGER_SECRET}`;
+  if (
+    !req.headers ||
+    !req.headers.authorization ||
+    req.headers.authorization !== expectedAuth
+  ) {
+    res.send("Unauthorized");
+  }
 
-app.get('/hackme', (_req, res) => {
-	log("GET on /hackme")
-	res.json({flag: flagService.getLastFlag()})
-})
+  if (req.body && req.body.id && req.body.vuln && req.body.flag) {
+    log(
+      `Received body with id: ${req.body.id} - vuln: ${req.body.vuln} - flag: ${req.body.flag}`
+    );
+    const id = req.body.id;
+    const vuln = req.body.vuln;
+    const flag = req.body.flag;
 
-log(`Starting Dummy ForcAD Vulnbox on port ${PORT}`)
-app.listen(PORT)
+    flagService.setFlag(id, vuln, flag);
+    res.send("Flag correctly set");
+  } else {
+    res.send("ERROR: flag not set");
+  }
+});
+
+app.get("/hackme", (_req, res) => {
+  log("GET on /hackme");
+  res.json({ flag: flagService.getLastFlag() });
+});
+
+log(`Starting Dummy ForcAD Vulnbox on port ${PORT}`);
+app.listen(PORT);
