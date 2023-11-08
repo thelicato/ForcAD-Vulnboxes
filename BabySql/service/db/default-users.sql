3d0c9506eeb814a6a4eaf2e0fc030d3e65d19c4a
thelicato <thelicato@users.noreply.github.com>
2023-11-08 16:35:11 +0100

    feat: update BabySQL to introduce flag-manager service removal

--- BabySql/service/db/default-users.sql
+++ BabySql/service/db/default-users.sql
@@ -1,2 +1,2 @@
 CREATE TABLE `babysql`.`users` (`id` INT NOT NULL , `username` VARCHAR(255) NOT NULL , `pwd` VARCHAR(255) NOT NULL ) ENGINE = InnoDB;
-INSERT INTO `users` (`id`, `username`, `pwd`) VALUES ('1', 'admin', 'CCIT{D3f4ult}');
+CREATE TABLE `babysql`.`flags` (`id` VARCHAR(255) NOT NULL , `vuln` VARCHAR(255) NOT NULL , `flag` VARCHAR(255) NOT NULL ) ENGINE = InnoDB;
