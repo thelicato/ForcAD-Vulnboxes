242ccbd108cb684817c8f925ca4d8349a1602b21
thelicato <thelicato@users.noreply.github.com>
2023-11-09 00:24:01 +0100

    feat: add token to dummy checker

--- Dummy/checker/service_lib.py
+++ Dummy/checker/service_lib.py
@@ -1,4 +1,6 @@
+import os
 import requests
+import yaml
 from checklib import *
 
 PORT = 5001
@@ -7,6 +9,14 @@ class CheckMachine:
 
     def __init__(self, checker):
         self.checker = checker
+        self.token = ''
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(f"{dir_path}/config.yml", "r") as stream:
+            try:
+                config = yaml.safe_load(stream)
+                self.token = config[checker.host]
+            except yaml.YAMLError as exc:
+                print(exc)
 
     def ping(self):
         r = requests.get(f'http://{self.checker.host}:{PORT}/ping', timeout=2)
@@ -16,6 +26,9 @@ class CheckMachine:
         new_id = rnd_string(10)
         r = requests.post(
             f'http://{self.checker.host}:{PORT}/put',
+            headers={
+                'Authorization': f"Bearer {self.token}"
+            },
             json={
                 'id': new_id,
                 'vuln': vuln,
@@ -30,6 +43,9 @@ class CheckMachine:
     def get_flag(self, flag_id, vuln):
         r = requests.get(
             f'http://{self.checker.host}:{PORT}/get',
+            headers={
+                'Authorization': f"Bearer {self.token}"
+            },
             params={
                 'id': flag_id,
                 'vuln': vuln,
